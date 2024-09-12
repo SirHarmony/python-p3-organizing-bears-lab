62c65133f8eb17f1ab827551cc19daf07969b4fe
Harmony <harmonymukolwe@gmail.com>
2024-09-12 21:10:07 +0300

    Completes lab

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate",34 , "M", "White", "Aggressive", TRUE);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy",12 , "F", "Black", "Cool", FALSE);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha",60 , "M", "Gold", "Reserved", TRUE);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown",42 , "F", "Cream", "Fierce", FALSE);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa",23 , "M", "Orange", "Dramatic", FALSE);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch",78 , "M", "Blue", "Aggressive", TRUE);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy",27 , "M", "Gray", "Fierce", TRUE);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL ,30 , "F", "White", "Loving", FALSE);
+
